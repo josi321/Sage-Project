519aaa2b1051b6b9dd7e8add003d5437d142ad08
Miquel Llabres <miquelllabres@gmail.com>
2017-03-23 10:50:19 -0400

    #archives

--- themes/sage/template-parts/content-events.php
+++ themes/sage/template-parts/content-events.php
@@ -14,19 +14,41 @@ if(is_single()) { ?>
 
 
 
-<article class="SingleEntrepeneur img-responsive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article class="SingleEntrepeneur " id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	<header class="entry-header" style="background:url(<?php the_post_thumbnail_url("large") ?> )center !important ; background-size:cover !important;"">
-	<div id="trapezoide" class="hidden-xs" style="background:<?php echo $color ?>" >
-		<?php the_title(); ?>
+	<div id="trapezoide" class="hide-for-small-only" style="background:<?php echo $color ?>" >
+		<div class="labelTitle">
+		<?php 
+		$test =	get_field('date');
+		$test = DateTime::createFromFormat("dmY",$test);
+		echo "<div class='date date-events' style='color:$color'><h2>".$test->format("d")."</h2>";
+		echo "<p>".$test->format("M")." ".$test->format("Y")."</p></div>";	
+			?>
+		<div>	
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php the_excerpt()  ?>
+		</div>
+		</div>
+		
 	</div>
-	<div id="triangle-bottomleft" class="hidden-xs" style="background: linear-gradient(to right top, <?php echo $color ?> 50%, transparent 50%);"></div>
+	<div id="triangle-bottomleft" class="hide-for-small-only" style="background: linear-gradient(to right top, <?php echo $color ?> 50%, transparent 50%);"></div>
 	</header>
-		<div id="trapezoid" class="visible-xs" style="background:<?php echo $color ?>">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php the_excerpt()  ?>
+		<div id="trapezoid" class="show-for-small-only" style="background:<?php echo $color ?>">
+		<div class="labelTitle">
+			<?php 
+		$test =	get_field('date');
+		$test = DateTime::createFromFormat("dmY",$test);
+		echo "<div class='date date-events' style='color:$color'><h2>".$test->format("d")."</h2>";
+		echo "<p>".$test->format("M")." ".$test->format("Y")."</p></div>";	
+			?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		</div>
+		<?php the_excerpt()  ?>
+		
 		</div>	
-		<div id="triangle-up-left" class="visible-xs" style="background: linear-gradient(to right bottom, <?php echo $color ?> 50%, transparent 50%);"></div>
+		<div id="triangle-up-left" class="show-for-small-only" style="background: linear-gradient(to right bottom, <?php echo $color ?> 50%, transparent 50%);">
+		</div>
 
 
 
@@ -42,14 +64,14 @@ if(is_single()) { ?>
 		<?php		}else{ ?>
 
 		
-<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6  entrepeneur events" id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?> style="background:url(<?php the_post_thumbnail_url("large") ?> )top right !important ; background-size:contain !important;"">
+<div class="small-12 medium-6 large-6 columns  entrepeneur events" id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?> style="background:url(<?php the_post_thumbnail_url("large") ?> )top right !important ; background-size:cover !important;"">
 	
 	<div id="triangle-bottomleft" style="background: linear-gradient(to right top, <?php echo $color ?> 50%, transparent 50%);">
 
 	<?php 
 		$test =	get_field('date');
 		$test = DateTime::createFromFormat("dmY",$test);
-		echo "<div class='date'><h1>".$test->format("d")."</h1>";
+		echo "<div class='date date-events'><h2>".$test->format("d")."</h2>";
 		echo "<p>".$test->format("M") .$test->format("Y")."</p></div>";	
 	?>
 
@@ -58,7 +80,7 @@ if(is_single()) { ?>
 		<h1><?php the_title() ?></h1>
 		<?php the_excerpt()  ?>
 		<a href="<?php echo get_permalink( $post->ID ); ?>">
-		READ MORE</a>
+		REGISTER</a>
 
 	</div>
 	
