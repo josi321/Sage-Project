3c85d64bfcaee56ccd4a488a78992ec1abf11246
Steph-Spencer <stephanie.e.spencer@gmail.com>
2017-03-27 14:28:53 -0400

    Program styling
    Made changes to styling of Program page

--- themes/sage/page-templates/page-program.php
+++ themes/sage/page-templates/page-program.php
@@ -7,29 +7,36 @@ get_header();?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<!--UPPER BANNER GOES HERE-->
+
+<!-- UPPER BANNER-->
+ <?php get_template_part( 'template-parts/upper-banner' ); ?>
+
+<!-- LEARNING SECTION-->
 
 <div id="program-learning" >
 	<h2 class="small-centered small-12"><?php the_field('learning_heading');?></h2>
-	<div class="card-wrap row">
-	<?php for($i=1; $i < 5; $i++) { ?>
-			<div class="card-front columns large-3 medium-6 small-12">
-				<img src="<?php the_field('card_'.$i.'_icon');?>">
-				<p><?php the_field('card_'.$i.'_front_text'); ?></p>
-			</div>
-			<div class="card-back columns large-3 medium-6 small-12">
-				<p><?php the_field('card_'.$i.'_back'); ?></p>
-			</div> 
-	<?php } ?>
+	<div class="card-wrap card-section flip-container" ontouchstart="this.classList.toggle('hover');">
+		<div class="flipper row">
+			<?php for($i=1; $i < 5; $i++) { ?>
+					<div class="card front columns large-3 medium-6 small-12">
+						<img src="<?php the_field('card_'.$i.'_icon');?>">
+						<p><?php the_field('card_'.$i.'_front_text'); ?></p>
+					</div>
+					<div class="card back columns large-3 medium-6 small-12">
+						<p><?php the_field('card_'.$i.'_back'); ?></p>
+					</div> 
+			<?php } ?>
+		</div>
 	</div>		
 </div>
+<!-- CALL TO ACTION SECTION-->
 <div id="program-cta">
-	<h2 class="small-centered small-12 column"><?php the_field('cta_heading'); ?><h2>
+	<h2 class="cta-lead small-12 column"><?php the_field('cta_heading'); ?></h2>
 	<div class="flourish small-centered">
 	</div>
-	<div class="small-4">
-		<p><?php the_field('cta_text_block_1');?><p>
-		<p><?php the_field('cta_text_block_2');?><p>
+	<div class="container row small-4">
+		<p class="column"><?php the_field('cta_text_block_1');?><p>
+		<p class="column"><?php the_field('cta_text_block_2');?><p>
 	</div>
 <div id="program-timeline">
 	<h2> <?php the_field('timeline_heading');?></h2>
@@ -39,7 +46,9 @@ get_header();?>
 	<p> <?php the_field( 'timeline_footer_text'); ?>
 </div>
 
-<!-- LOWER BANNER GOES HERE-->
+<!-- LOWER BANNER-->
+<?php get_template_part( 'template-parts/lower-banner' ); ?>
+
 
 <?php endwhile; ?>
 <?php get_footer(); ?>
