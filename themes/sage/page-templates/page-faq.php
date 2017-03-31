4b6ae18cbc9913ae953eca699d58b771dfff5939
Steph-Spencer <stephanie.e.spencer@gmail.com>
2017-03-31 09:39:55 -0400

    FAQ complete
    Added loop to populate content to FAQ accordions and finished styling.

--- themes/sage/page-templates/page-faq.php
+++ themes/sage/page-templates/page-faq.php
@@ -10,34 +10,24 @@ get_header();?>
 <h2 class="faq-title small-centered"><?php the_title(); ?></h2>
 <div class="flourish small-centered"></div>
 
-<!-- ACCORDION -->
-<!-- <h2 id="accordion" class="docs-heading" data-magellan-target="accordion"><a href="#accordion"></a>Frequently Asked Questions</h2> -->
-					<ul class="accordion" data-accordion role="tablist">
+<?php 
+$args = array ("post_type"=>"faq","posts_per_page" => -1); 
+$faq_query = new WP_Query ($args); 
+if ($faq_query->have_posts()): ?>
+
+					<ul id="the-faqs" class="accordion faq small-12 medium-6 small-4" data-accordion role="tablist">
+<?php $i=1; while($faq_query->have_posts()):$faq_query->the_post(); ?>
 					  <li class="accordion-item" data-accordion-item>
 					    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
-					    <a href="#panel1d" role="tab" class="faq-question accordion-title" id="panel1d-heading" aria-controls="panel1d">Accordion 1</a>
+					    <a href="#panel<?php echo $i?>d" role="tab" class="faq-question accordion-title" id="panel1d-heading" aria-controls="panel1d"><?php the_field('question'); ?></a>
 					    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
-					    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
-					      Panel 1. Lorem ipsum dolor
+					    <div id="panel<?php echo $i?>d" class="accordion-content faq-answer" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
+					      <?php the_field('answer'); ?>
 					    </div>
 					  </li>
-					  <li class="accordion-item" data-accordion-item>
-					    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
-					    <a href="#panel1d" role="tab" class="faq-question accordion-title" id="panel1d-heading" aria-controls="panel1d">Accordion 2</a>
-					    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
-					    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
-					      Panel 2. Lorem ipsum dolor
-					    </div>
-					  </li>
-					  <li class="accordion-item" data-accordion-item>
-					    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
-					    <a href="#panel1d" role="tab" class="faq-question accordion-title" id="panel1d-heading" aria-controls="panel1d">Accordion 3</a>
-					    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
-					    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
-					      Panel 3. Lorem ipsum dolor
-					    </div>
-					  </li>
+		<?php $i++; endwhile; ?>			  
 					</ul>
+		<?php endif; ?>
 
 <!-- <?php endwhile; ?> -->
 <?php get_footer(); ?>
